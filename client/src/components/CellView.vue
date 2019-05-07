5e20a8e6a79c3324d998e47d9bce922110f9b4b8
mokunshao <1627510957@qq.com>
2019-05-07 15:21:55 +0800

    备份

---
+++ client/src/components/CellView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="cellWrapper">
+    <div class="imgWrapper">
+      <img :src="momentsObj.img" alt>
+    </div>
+    <div class="contentWrapper">
+      <div class="cellName">{{momentsObj.name}}</div>
+      <div class="cellText" v-if="momentsObj.text">{{momentsObj.text}}</div>
+      <div class="cellImgs" v-if="momentsObj.imgs.length > 0">
+        <img v-for="(item,index) in momentsObj.imgs" :key="index" :src="item" alt>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CellView",
+  props: {
+    momentsObj: Object
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.cellWrapper {
+  box-sizing: border-box;
+  padding: 16px;
+  border-bottom: 1px solid #d9d9d9;
+  display: flex;
+  flex-direction: row;
+  .imgWrapper {
+    flex: 1;
+    img {
+      width: 40px;
+      height: 40px;
+    }
+  }
+  .contentWrapper {
+    flex: 9;
+    padding-left: 8px;
+    .cellName {
+      font-weight: bold;
+      color: #8cafe6;
+    }
+    div {
+      // margin-top: 8px;
+      word-wrap: break-word;
+    }
+    .cellImgs {
+      // width: 100%;
+      // overflow: hidden;
+      img {
+        width: 31%;
+        height: 6rem;
+        float: left;
+        margin-right: 5px;
+        margin-bottom: 5px;
+      }
+    }
+  }
+}
+</style>
--- client/src/views/Moments.vue
+++ client/src/views/Moments.vue
@@ -2,34 +2,91 @@
   <div class="moments">
     <Header title="朋友圈" btnIcon="camera" :hasLeft="true"></Header>
     <div class="container">
-      <img :src="user.avatar" :alt="user.avatar">
-      {{user.username}}
+      <div class="headWrapper">
+        <div class="userInfo">
+          <span class="username">{{user.username}}</span>
+          <div class="userImg">
+            <img :src="user.avatar" :alt="user.username">
+          </div>
+        </div>
+      </div>
+      <div class="contentWrapper">
+        <CellView v-for="(moment,index) in momentsList" :key="index" :momentsObj="moment"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Header from "../components/Header";
+import CellView from "../components/CellView";
 import jwt_decode from "jwt-decode";
+
 export default {
   name: "Moments",
-  components: { Header },
+  components: { Header, CellView },
+  data() {
+    return {
+      momentsList: {}
+    };
+  },
   computed: {
     user() {
       const token = localStorage.wxToken;
       const decoded = jwt_decode(token);
-      console.log(decoded);
       return decoded;
     }
+  },
+  methods: {
+    getLatestData() {
+      this.$axios("/api/moment/latest").then(res => {
+        this.momentsList = res.data;
+      });
+    }
+  },
+  mounted() {
+    this.getLatestData();
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.container {
-  width: 100%;
-  height: calc(100% - 50px);
-  padding-top: 50px;
-  overflow: auto;
+.moments {
+  height: 100%;
+  .container {
+    height: calc(100% - 50px);
+    padding-top: 50px;
+    overflow: auto;
+    .headWrapper {
+      position: relative;
+      height: 200px;
+      background: url(../assets/cricle_bg.png) no-repeat;
+      background-size: 100% 100%;
+      .userInfo {
+        position: absolute;
+        bottom: -30px;
+        right: 16px;
+        .userImg {
+          width: 60px;
+          height: 60px;
+          padding: 3px;
+          border: 1px solid #c5c5c5;
+          box-sizing: border-box;
+          img {
+            width: 100%;
+            height: 100%;
+          }
+        }
+        .username {
+          position: absolute;
+          right: 76px;
+          color: white;
+        }
+      }
+    }
+    .contentWrapper {
+      // margin-top: 30px;
+    }
+  }
 }
 </style>
